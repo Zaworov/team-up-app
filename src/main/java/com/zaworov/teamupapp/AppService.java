80da892fe126070f4c9a83a78a0b89d730aba185
Zaworov <zavvorov@gmail.com>
2023-02-20 22:27:41 +0100

    VT-7 Removed unecessery methods

--- src/main/java/com/zaworov/teamupapp/AppService.java
+++ src/main/java/com/zaworov/teamupapp/AppService.java
@@ -2,29 +2,12 @@ package com.zaworov.teamupapp;
 
 import com.zaworov.teamupapp.domain.game.TeamCreationMode;
 import com.zaworov.teamupapp.domain.game.VolleyballGame;
-import com.zaworov.teamupapp.domain.player.PlayerTestDataHelper;
 import com.zaworov.teamupapp.domain.player.VolleyballPlayer;
 import com.zaworov.teamupapp.domain.score.VolleyballSet;
 
 import java.util.ArrayList;
 
 public class AppService {
-    public static void Main(String[] args) {
-        ;
-    }
-
-    public void playGame() {
-        //Create players
-        ArrayList<VolleyballPlayer> players = PlayerTestDataHelper.createFullSquadForVolleyballGame();
-        VolleyballGame volleyballGame = createGameAndAssignPlayersRandomly(players);
-
-        playSet(volleyballGame, new VolleyballSet(1, 10, 25));
-        playSet(volleyballGame, new VolleyballSet(2, 10, 25));
-        playSet(volleyballGame, new VolleyballSet(3, 10, 25));
-
-        volleyballGame.updatePlayersWithFinishedGame();
-    }
-
     public void playSet(VolleyballGame volleyballGame, VolleyballSet set) {
         volleyballGame.addSet(set);
         volleyballGame.addTeamWin(set);
